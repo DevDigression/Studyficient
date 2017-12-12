93c8c0105bcf7ff6402039574f438f48281bfbaf
Sebastian Bastidas <sbastidasr@gmail.com>
2017-12-12 18:00:56 -0500

    login error

--- public/login.js
+++ public/login.js
@@ -1,28 +1,42 @@
 $(() => {
 	$('#login-form').submit((event) =>{
 		event.preventDefault();
-
 		let userData = {
 			username: $('#login-username').val(),
 			password: $('#login-password').val()
 		};
-
-	    $.ajax({
-	        url: `/api/auth/login`,
-	        type: "POST",
-	        data: JSON.stringify(userData),
-	        contentType: "application/json; charset=utf-8",
-	        dataType: "json",
-	        success: function(data){
-	            // data.authToken;
-	            console.log("Success!");
-	            localStorage.setItem("token", data.authToken);
-	            window.location = "/studyspace.html";
-	        },
-	        error: function(errorData){
-	        // do error stuff
-	        },
-	    });
-
+		login (userData)
 	});
+
+	$('#demo-btn').click((event) =>{
+		let userData = {
+			username: "test",
+			password: "test12345"
+		};
+		login (userData)
+	});
+
 });
+
+
+function login (userData){
+
+
+	$.ajax({
+		url: `/api/auth/login`,
+		type: "POST",
+		data: JSON.stringify(userData),
+		contentType: "application/json; charset=utf-8",
+		dataType: "json",
+		success: function(data){
+			// data.authToken;
+			console.log("Success!");
+			localStorage.setItem("token", data.authToken);
+			window.location = "/studyspace.html";
+		},
+		error: function(errorData){
+			 $("#error-message").text("Invalid username or password")
+			// $("#error-message").text("Error: " + errorData.responseText)
+		},
+	});
+}
